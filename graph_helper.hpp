ffb4ed1c8a7d647bfc9f3fa8aa20353a2c0a308a
PurplefinNeptuna <ilhamaryasuta@gmail.com>
2019-03-16 10:55:46 +0700

    another commit

--- graph_helper.hpp
+++ graph_helper.hpp
@@ -7,46 +7,7 @@
 #include <vector>
 using namespace std;
 
-inline void buildGraph(string name, const vector<double>& x, const vector<double>& y, bool logx, bool logy, double xmin, double xmax, double ymin, double ymax, string title, string additionalLineStyle, string color) {
-	Gnuplot plot("lines " + additionalLineStyle + " lc rgb \"" + color + "\"");
-	plot.cmd("set output");
-	plot.cmd("set term qt title \"" + title + "\"");
-	plot.set_title(title);
-	plot.set_grid();
-	if (logx == true) {
-		plot.set_xlogscale();
-	} else {
-		plot.set_xrange(xmin, xmax);
-	}
-
-	if (logy == true) {
-		plot.set_ylogscale();
-	} else {
-		plot.set_yrange(ymin, ymax);
-	}
-
-	plot.plot_xy(x, y, name);
-
-	Gnuplot plotpng("lines " + additionalLineStyle + " lc rgb \"" + color + "\"");
-	plotpng.cmd("set term png\nset output \"" + name + ".png\"");
-	plotpng.set_title(title);
-	plotpng.set_grid();
-	if (logx == true) {
-		plotpng.set_xlogscale();
-	} else {
-		plotpng.set_xrange(xmin, xmax);
-	}
-
-	if (logy == true) {
-		plotpng.set_ylogscale();
-	} else {
-		plotpng.set_yrange(ymin, ymax);
-	}
-
-	plotpng.plot_xy(x, y, name);
-}
-
-inline void pngGraph(string name, const vector<double>& x, const vector<double>& y, bool logx, bool logy, double xmin, double xmax, double ymin, double ymax, string title, string additionalLineStyle, string color, string lineName) {
+inline void pngGraph(string name, const vector<double>& x, const vector<double>& y, bool logx, bool logy, double xmin, double xmax, double ymin, double ymax, string title, string additionalLineStyle, string color, string lineName = "") {
 	if (lineName == "")
 		lineName = name;
 	Gnuplot* plotpng = new Gnuplot();
@@ -69,4 +30,29 @@ inline void pngGraph(string name, const vector<double>& x, const vector<double>&
 	plotpng->plot_xy(x, y, lineName);
 	delete plotpng;
 }
+
+inline void buildGraph(string name, const vector<double>& x, const vector<double>& y, bool logx, bool logy, double xmin, double xmax, double ymin, double ymax, string title, string additionalLineStyle, string color) {
+	Gnuplot* plot = new Gnuplot();
+	plot->cmd("set output");
+	plot->cmd("set term qt title \"" + title + "\"");
+	plot->set_style("lines " + additionalLineStyle + " lc rgb \"" + color + "\"");
+	plot->set_title(title);
+	plot->set_grid();
+	if (logx == true) {
+		plot->set_xlogscale();
+	} else {
+		plot->set_xrange(xmin, xmax);
+	}
+
+	if (logy == true) {
+		plot->set_ylogscale();
+	} else {
+		plot->set_yrange(ymin, ymax);
+	}
+
+	plot->plot_xy(x, y, name);
+
+	pngGraph(name, x, y, logx, logy, xmin, xmax, ymin, ymax, title, additionalLineStyle, color);
+}
+
 #endif
